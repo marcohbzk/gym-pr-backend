52f9a2e2ba29a083b0c80e3ed61dbefe0e998ae7
marcohbzk <2212631@my.ipleiria.pt>
2025-05-20 21:15:56 +0100

    Middleware and Prs [CRUD]
    authMiddleware.js created and logic complete, prs.js crud and logic complete.
    All routes working.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
-const authRoutes = require("./routes/auth");
-// const { PrismaClient } = require("@prisma/client");
 const app = express();
-// const prisma = new PrismaClient();
+
+// Route constants
+const authRoutes = require("./routes/auth");
+const prsRoutes = require("./routes/prs");
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/auth", authRoutes);
+app.use("/prs", prsRoutes);
 
 app.get("/", (req, res) => res.send(`API WORKING`));
 
